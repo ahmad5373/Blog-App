e4cba164336bfba0764f283888060217e0e1c9a2
ahmad5373 <ahmadraxa550@gmail.com>
2024-09-26 19:30:09 +0500

    update navbar  functionality

--- src/components/BlogNavbar.js
+++ src/components/BlogNavbar.js
@@ -3,42 +3,50 @@ import { Link } from 'react-router-dom';
 import '../index.css';
 
 const BlogNavbar = ({ isAuthenticated, handleLogout}) => {
-    return (
-      <>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+  return (
+    <>
+     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <Link className="navbar-brand ml-2" to="/">Blog Posts</Link>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
         <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
-          <div className="d-flex justify-content-between w-100">
-            <ul className="navbar-nav">
+          <ul className="navbar-nav mr-auto">
             <li className="nav-item">
-                <Link className="nav-link navbar-brand" to="/">Blog Posts</Link>
-              </li>
-            </ul>
-            <ul className="navbar-nav mr-4">
-              {isAuthenticated ? (
-                <>
-                  <li className="nav-item ">
-                    <button className="btn btn-outline-light custom-margin-right" onClick={handleLogout}>Logout</button>
-                  </li>
-                </>
-              ) : (
-                <>
-                  <li className="nav-item ">
-                    <Link className="btn btn-outline-light custom-margin-right" to="/login">Login</Link>
-                  </li>
-                  <li className="nav-item mr-12">
-                    <Link className="btn btn-outline-light custom-margin-right" to="/register">Register</Link>
-                  </li>
-                </>
-              )}
-            </ul>
-          </div>
+            </li>
+          </ul>
+
+          <ul className="navbar-nav ml-auto">
+            {isAuthenticated ? (
+              <>
+                <li className="nav-item">
+                  <button className="btn btn-outline-light m-2" onClick={handleLogout}>Logout</button>
+                </li>
+              </>
+            ) : (
+              <>
+                <li className="nav-item">
+                  <Link className="btn btn-outline-light m-2" to="/login">Login</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="btn btn-outline-light m-2" to="/register">Register</Link>
+                </li>
+              </>
+            )}
+          </ul>
         </div>
       </nav>
-      </>
-    );
+    </>
+  );  
   };
 
 export default BlogNavbar;
